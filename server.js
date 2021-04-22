8440f0d129ee09d5e5b59db6c3594cab373a5d89
justinrds2001 <justinrodrigues@live.nl>
2021-04-22 13:50:00 +0200

    Express implemented and UC-103 added

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const http = require('http')
-
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
+app.get('/', (req, res) => {
   let result = {
-      'response': 'Hello World!',
-      'status': 'Alles OK!'
+    'response': 'Hello World!',
+    'status': 'Alles OK!'
   }
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(result))
+  res.json(result)
 })
 
-server.listen(port, () => {
-  console.log(`Server running at http://:${port}/`)
+// UC-103 Systeeminfo opvragen
+app.get('/system', (req, res) => {
+  let result = {
+    name: 'Justin Rodrigues da Silva',
+    studentNr: 2144403,
+    description: 'This is a api that returns information about food'
+  }
+  res.json(result)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
